ff030fb63834bca9ca816953d742257a0808461f
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-01-26 14:52:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,14 +4,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  end
  
   elsif int % 3 == 0
     return "Fizz"
-  end
     
   elsif int % 5 == 0
     return "Buzz"
   end
-    
 end
