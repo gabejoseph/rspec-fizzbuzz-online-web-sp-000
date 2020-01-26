9bc2e92e05d5814f824f61d6dde10ddd6bfee4cb
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-01-26 14:41:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,12 @@ require_relative '.spec/fizzbuzz_spec.rb'
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-     return "Fizz"
+  if int % 3 && int % 5
+    return "FizzBuzz"
+  elsif int % 3 
+    return "Fizz"
+  elsif int % 5
+    return "Buzz"
+  
   end
 end
